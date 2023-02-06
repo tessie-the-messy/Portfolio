667d2e86baf63cb4ed32e5a87d6614e1a139b2cb
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-02-05 20:16:45 -0700

    Fixed routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import { HashRouter as Router, Route, Routes} from "react-router-dom"
 
 import AboutMe from "./components/Pages/AboutMe"
 import Projects from "./components/Pages/Projects"
