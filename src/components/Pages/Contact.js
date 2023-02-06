d2ebce2caed7c843db7476ac29ca03cc6bb481b4
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-02-05 22:05:11 -0700

    Completed with react refactoring

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,26 +1,27 @@
 import React from "react";
 import Header from "../header";
 import Footer from "../footer";
+import "../styles/Contact.css"
 
 const Contact = () => {
   return (
     <div>
       <Header />
       <div>
-        <h2 id="contact">Contact</h2>
+        <h2>Contact</h2>
         <section>
-          <ul className="info">
-            <li className="abc">
-              <a href="#">555.555.5555</a>
+          <ul className="contact">
+            <li className="contactLi">
+              Phone Number: <a href="#">555.555.5555</a>
             </li>
-            <li className="abc">
-              <a href="#">E-mail</a>
+            <li className="contactLi">
+              E-mail:<a href="#">tessa.elise.robinson@gmail.com</a>
             </li>
-            <li className="abc">
-              <a href="https://github.com/tessie-the-messy">GitHub</a>
+            <li className="contactLi">
+              Github: <a href="https://github.com/tessie-the-messy">github.com/tessie-the-messy</a>
             </li>
-            <li className="abc">
-              <a href="www.linkedin.com/in/tessa-elise-robinson">LinkedIn</a>
+            <li className="contactLi">
+              LinkedIn: <a href="www.linkedin.com/in/tessa-elise-robinson">linkedin.com/in/tessa-elise-robinson</a>
             </li>
           </ul>
         </section>
--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../header";
 import Footer from "../footer";
+import prepUrself from "../../assets/prepUrself.png";
+import hogwarts from "../../assets/hogwarts.png";
+import devLog from "../../assets/devLog.png";
+import "../styles/Projects.css";
 
 const Projects = () => {
   return (
@@ -18,33 +22,21 @@ const Projects = () => {
         <section className="images">
           <h3>prepUrself</h3>
           <a href="https://bencha27.github.io/prepUrself/">
-            <img
-              className="small-image"
-              src="https://images.unsplash.com/photo-1470165344182-24727c18c317?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
-              alt
-            />
+            <img src={prepUrself} alt="prepUrself site preview" />
           </a>
           <h3>Hogwarts Enrollment Portal</h3>
           <a href="http://lit-castle-55469.herokuapp.com/">
-            <img
-              className="small-image"
-              src="https://images.unsplash.com/photo-1633478062482-790e3b5dd810?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-              alt
-            />
+            <img src={hogwarts} alt="Hogwarts Enrollment Portal site preview" />
           </a>
           <h3>dev.log</h3>
           <a href="http://https://dev-log.herokuapp.com/">
-            <img
-              className="small-image"
-              src="https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt
-            />
+            <img src={devLog} alt="dev.log site preview" />
           </a>
-          <img
+          {/* <img
             className="small-image"
             src="https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
             alt
-          />
+          /> */}
         </section>
       </div>
       <Footer />
