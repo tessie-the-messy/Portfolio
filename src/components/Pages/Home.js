1d5126ff6b40d48da56cc3820caeadbdacbaa032
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-02-07 01:35:49 -0700

    Added basic HTML for Home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,10 +1,34 @@
 import React from "react";
 import Header from "../header";
+import "../styles/Home.css"
 
 const Home = () => {
- return (
+  return (
     <div>
-        
+      <Header />
+      <div>
+        <h1>Tessa</h1>
+        <h1>Robinson</h1>
+      </div>
+      <div className="cardContainer">
+        <button className="card">
+          <div className="top">
+            <h3>About Me</h3>
+          </div>
+        </button>
+        <button>
+          <div className="top">
+            <h3>Projects</h3>
+          </div>
+        </button>
+        <button>
+          <div className="top">
+            <h3>Contact</h3>
+          </div>
+        </button>
+      </div>
     </div>
- )
+  );
 };
+
+export default Home;
