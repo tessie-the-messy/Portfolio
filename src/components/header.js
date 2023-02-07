dd50949745e1bc5e82e5dd8c5446a111a15b23d4
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-02-07 02:38:51 -0700

    Finished styling header. Need to add header functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import Navigation from './navigation';
+import React from "react";
+import Navigation from "./navigation";
 
 const Header = () => {
-    return(
+  return (
     <header>
-        <h1>Tessa Robinson</h1>
-        <Navigation />
+      <img
+        className="headerX"
+        src={require("../assets/x.png")}
+        alt="'x' shaped close button image, doesn't actually do anything."
+      />
+      <div className="navUrl">
+        <select name="nav">
+          <option value="" disabled selected hidden>www.WelcomeToMyPortfolio.com/choose-a-page</option>
+          <option value="home">Home</option>
+          <option value="aboutMe">About Me</option>
+          <option value="projects">Projects</option>
+          <option value="contact">Contact</option>
+        </select>
+      </div>
     </header>
-    );
+  );
 };
 
 export default Header;
