65c7aa86ca940e572983144a38a60c00acc66530
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-02-20 14:11:34 -0700

    finished top div of header, working on nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,33 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 const Header = () => {
   return (
-
-    <header className="container-fluid">
-      <img
-        className="headerX"
-        src={require("../assets/x.png")}
-        alt="'x' shaped close button image, doesn't actually do anything."
-        />
-      <div className="fakeUrl">
-        <div name="fakeUrlText">
-          www.WelcomeToMyPortfolio.com/choose-a-page
+    <header>
+      <div className="topBox d-flex">
+        <h2 className="p-2 w-100 welcome">**~Welcome~**</h2>
+        <div className="p-2 flex-shrink-1">
+          <img
+            className="headerX"
+            src={require("../assets/x.png")}
+            alt="'x' shaped close button, doesn't actually do anything."
+          />
         </div>
       </div>
+      <div className="navBox">
+        <nav>
+          <ul className="d-md-inline-flex">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+      <div className="fakeUrl">
+        <h2 className="fakeUrlText">www.WelcomeToMyPortfolio.com</h2>
+      </div>
     </header>
-       
   );
 };
 
