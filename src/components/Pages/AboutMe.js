289650d4d275e75ebbcf6515b2aa9a6dfbccc6bc
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-02-27 11:00:38 -0700

    added content and some design to About Me page

--- src/components/Pages/AboutMe.js
+++ src/components/Pages/AboutMe.js
@@ -1,5 +1,5 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Header from "../header";
 import Footer from "../footer";
 import "../styles/AboutMe.css";
@@ -11,29 +11,66 @@ const AboutMe = () => {
       <Header />
       <div>
         <h1 className="title">About Me</h1>
-        <div className="about">
-          <p>
-            My name is Tessa, and I'm currently studying to be a web developer
-            through DU's Full-Stack Flex program. I recieved my B.A. from the
-            University of Northern Colorado's Acting Department. Around the time
-            the Pandemic started, I decided I wanted to take a different path in
-            life, and 2 1/2 years later, found myself here! I am an avid gamer,
-            typically playing Dead by Daylight, Valorant, and various graphic
-            adventures.
-          </p>
+        <div className="container">
+          <div className="card">
+            <div className="card-header">
+              <div className="d-flex justify-content-between">
+                <h2 className="card-title">Hello, my name is...</h2>
+                <img
+                  className="cardX flex-end"
+                  src={require("../../assets/x.png")}
+                  alt="'x' shaped close button, doesn't actually do anything."
+                />
+              </div>
+            </div>
+            <div className="card-body">
+              <p>
+                Tessa!! I'm a recent graduate of DU's Full-Stack Flex
+                certificate program. I recieved my B.A. from the University of
+                Northern Colorado's Acting Department. Around the time the
+                Pandemic started, I decided I wanted to take a different path in
+                life, and 2 1/2 years later, found myself here! I am an avid
+                gamer, typically playing Dead by Daylight, Stardew Valley, and
+                various graphic adventures.
+              </p>
+            </div>
+          </div>
+          <div className="container">
+            <div className="card homeCard">
+              <div className="card-header">
+                <div className="d-flex justify-content-between">
+                  <h2 className="card-title">Skills</h2>
+                  <img
+                    className="cardX flex-end"
+                    src={require("../../assets/x.png")}
+                    alt="'x' shaped close button, doesn't actually do anything."
+                  />
+                </div>
+              </div>
+              <div className="card-body">
+                <ul className="skills">
+                  <li>HTML and CSS</li>
+                  <li>JavaScript</li>
+                  <li>Git</li>
+                  <li>Third-party APIs (jQuery, Bootstrap)</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card homeCard">
+              <div className="card-header">
+                <div className="d-flex justify-content-between">
+                  <h2 className="card-title">Education</h2>
+                  <img
+                    className="cardX flex-end"
+                    src={require("../../assets/x.png")}
+                    alt="'x' shaped close button, doesn't actually do anything."
+                  />
+                </div>
+              </div>
+              <div className="card-body"></div>
+            </div>
+          </div>
         </div>
-        <h2 id="skills">Skills</h2>
-        <section>
-          <p>Upon graduation, I will have learned:</p>
-          <ul className="skills">
-            <li>HTML and CSS</li>
-            <li>Git</li>
-            <li>JavaScript</li>
-            <li>Third-party APIs (jQuery, Bootstrap)</li>
-            <li>Server-side APIs, AJAX, and JSON</li>
-            <br />
-          </ul>
-        </section>
       </div>
       <Footer />
     </div>
