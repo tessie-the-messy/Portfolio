395c905edce976f7c31dc411d8237969d5a1c6da
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-03-07 23:05:04 -0700

    Built out form on contact page w/o styling

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "../header";
-import Footer from "../footer";
+// import Footer from "../footer";
 import "../styles/Contact.css";
 import "../../App.css";
 
@@ -11,64 +11,84 @@ const Contact = () => {
       <Header />
       <div>
         <h1 className="title">Contact</h1>
-        <div className="container">
+        <div className="container d-flex itemsContainer">
           <div className="container">
             <div className="card">
               <div className="card-header">
-                <div className="d-flex">
-                  <h2 className="card-title">LinkedIn</h2>
-                  <img
-                    className="cardX flex-end"
-                    src={require("../../assets/x.png")}
-                    alt="'x' shaped close button, doesn't actually do anything."
-                  />
-                </div>
+                <h2 className="card-title">LinkedIn</h2>
               </div>
               <div className="card-body"></div>
             </div>
             <div className="card">
               <div className="card-header">
-                <div className="d-flex">
-                  <h2 className="card-title">Github</h2>
-                  <img
-                    className="cardX flex-end"
-                    src={require("../../assets/x.png")}
-                    alt="'x' shaped close button, doesn't actually do anything."
-                  />
-                </div>
+                <h2 className="card-title">Github</h2>
               </div>
               <div className="card-body"></div>
             </div>
             <div className="card">
               <div className="card-header">
-                <div className="d-flex">
-                  <h2 className="card-title">E-mail</h2>
-                  <img
-                    className="cardX flex-end"
-                    src={require("../../assets/x.png")}
-                    alt="'x' shaped close button, doesn't actually do anything."
-                  />
-                </div>
+                <h2 className="card-title">E-mail</h2>
               </div>
               <div className="card-body"></div>
             </div>
           </div>
-          <div className="card">
+          <form className="card col-8">
             <div className="card-header">
-              <div className="d-flex">
-                <h2 className="card-title">Or, Reach out here!</h2>
-                <img
-                  className="cardX flex-end"
-                  src={require("../../assets/x.png")}
-                  alt="'x' shaped close button, doesn't actually do anything."
-                />
+              <h2 className="card-title">Or, Reach out here!</h2>
+            </div>
+            <div className="card-body">
+              <div class="form-group d-flex align-items-center justify-content-between">
+                <label for="firstname" class="mr-3">
+                  First Name:
+                </label>
+                <input id="firstname" type="text" class="form-control w-75" />
+              </div>
+              <div class="form-group d-flex align-items-center justify-content-between">
+                <label for="lastname" class="mr-3">
+                  Last Name:
+                </label>
+                <input id="lastname" type="text" class="form-control w-75" />
+              </div>
+              <div class="form-group d-flex align-items-center justify-content-between">
+                <label for="email" class="mr-3">
+                  E-mail:
+                </label>
+                <input id="email" type="text" class="form-control w-75" />
+              </div>
+              <div class="form-group d-flex align-items-center justify-content-between">
+                <label for="company" class="mr-3">
+                  Company:
+                </label>
+                <input id="company" type="text" class="form-control w-75" />
+              </div>
+              <div class="form-group d-flex align-items-center justify-content-between">
+                <label for="company" class="mr-3">
+                  Reason for reaching out:
+                </label>
+                <select
+                  class="form-control w-75"
+                  aria-label="Default select example"
+                >
+                  <option selected>Choose a reason:</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div class="form-group d-flex align-items-center justify-content-between">
+                <label for="message" class="mr-3">
+                  Message:
+                </label>
+                <textarea
+                  class="form-control w-75"
+                  id="message"
+                  rows="3"
+                ></textarea>
               </div>
             </div>
-            <div className="card-body"></div>
-          </div>
+          </form>
         </div>
       </div>
-      <Footer />
     </div>
   );
 };
--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "themesberg/windows-95-ui-kit/css/w95.css";
 import Header from "../header";
-import Footer from "../footer";
+// import Footer from "../footer";
 import "../styles/Projects.css";
 import "../../App.css";
 
@@ -11,45 +12,45 @@ const Projects = () => {
       <Header />
       <div>
         <h1 className="title">Projects</h1>
-        <div className="container">
-          <div className="card projectCard">
-            <div className="card-header d-flex">
-              <h2 className="card-title">dev.log</h2>
-              <img
-                className="cardX flex-end"
-                src={require("../../assets/x.png")}
-                alt="'x' shaped close button, doesn't actually do anything."
-              />
+        <div className="itemsContainer container d-flex flex-column justify-content-sm-around">
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title mb-4 mb-lg-1 fs-4">dev.log</h3>
             </div>
-            <div className="card-body"></div>
+            <div className="card-body">
+              <div className="card-text"></div>
+              <div class="d-flex justify-content-end mt-3">
+                <button class="btn mr-2 border-dark"
+                    type="button"><span class="btn-text">Check it out!</span></button>
+            </div>
+            </div>
           </div>
-          <div className="card projectCard">
-            <div className="card-header d-flex">
-              {/* <div className=""> */}
-                <h2 className="card-title">Hogwarts Enrollment Portal</h2>
-                <img
-                  className="cardX flex-end"
-                  src={require("../../assets/x.png")}
-                  alt="'x' shaped close button, doesn't actually do anything."
-                />
-              {/* </div> */}
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title mb-4 mb-lg-1 fs-4">Hogwarts Enrollment Portal</h3>
             </div>
-            <div className="card-body"></div>
+            <div className="card-body">
+            <div className="card-text"></div>
+            <div class="d-flex justify-content-end mt-3">
+                <button class="btn mr-2 border-dark"
+                    type="button"><span class="btn-text">Check it out!</span></button>
+            </div>
+            </div>
           </div>
-          <div className="card projectCard">
-            <div className="card-header d-flex">
-              <h2 className="card-title">prepUrself</h2>
-              <img
-                className="cardX flex-end"
-                src={require("../../assets/x.png")}
-                alt="'x' shaped close button, doesn't actually do anything."
-              />
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title mb-4 mb-lg-1 fs-4">prepUrself</h3>
             </div>
-            <div className="card-body"></div>
+            <div className="card-body">
+            <div className="card-text"></div>
+            <div class="d-flex justify-content-end mt-3">
+                <button class="btn mr-2 border-dark"
+                    type="button"><span class="btn-text">Check it out!</span></button>
+            </div>
+            </div>
           </div>
         </div>
       </div>
-      <Footer />
     </div>
   );
 };
